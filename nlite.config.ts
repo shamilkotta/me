6beb6cd998081fc1633b64e23b2a4df4959f14bd
shamilkotta <shamilkotta99@gmail.com>
2026-05-29 23:59:55 +0530

    feat: deployment setup

--- nlite.config.ts
+++ nlite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "nlite/config";
 import path from "node:path";
+import {cloudflare} from "nlite/adapters"
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), cloudflare()],
   resolve: {
     alias: {
       "@": path.resolve(process.cwd()),
